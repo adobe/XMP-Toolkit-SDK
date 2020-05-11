3154c9a0ca3df515d3acd4a9cddab50641f0c662
pawankishorsingh <pksingh@adobe.com>
2020-05-11 21:19:02 +0530

    Changed Adobe Confidential to BSD-Clause-3 header. (#6)
    Changed Adobe Confidential to BSD-Clause-3 header.

--- source/SafeStringAPIs.h
+++ source/SafeStringAPIs.h
@@ -1,20 +1,12 @@
-/*************************************************************************
-*
-* ADOBE CONFIDENTIAL
-* ___________________
-*
-*  Copyright 2010 Adobe
-*  All Rights Reserved.
-*
-* NOTICE:  All information contained herein is, and remains
-* the property of Adobe and its suppliers,
-* if any.  The intellectual and technical concepts contained
-* herein are proprietary to Adobe and its
-* suppliers and are protected by trade secret or copyright law.
-* Dissemination of this information or reproduction of this material
-* is strictly forbidden unless prior written permission is obtained
-* from Adobe.
-**************************************************************************/
+// =================================================================================================
+// Copyright 2020 Adobe
+// All Rights Reserved.
+// NOTICE: Adobe permits you to use, modify, and distribute this file in
+// accordance with the terms of the Adobe license agreement accompanying
+// it. If you have received this file from a source other than Adobe,
+// then your use, modification, or distribution of it requires the prior
+// written permission of Adobe. 
+// =================================================================================================
 
 //Safe String APIs Version 1.0
 
