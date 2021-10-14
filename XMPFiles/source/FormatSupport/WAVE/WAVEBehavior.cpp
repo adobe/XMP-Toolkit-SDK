d1bd0d4896925e5d7036128091895eacf5f83203
arpitapanda <arpanda@adobe.com>
2021-10-14 12:13:10 +0530

    Fixing CTECHXMP-4170632

--- XMPFiles/source/FormatSupport/WAVE/WAVEBehavior.cpp
+++ XMPFiles/source/FormatSupport/WAVE/WAVEBehavior.cpp
@@ -608,11 +608,10 @@ bool WAVEBehavior::parseDS64Chunk( const Chunk& ds64Chunk, WAVEBehavior::DS64& d
 		memcpy( &ds64, data, kMinimumDS64ChunkSize );
 
 		// If there is more data but the table length is <= 0 then this is not a valid ds64 chunk
-		if( size > kMinimumDS64ChunkSize && ds64.tableLength > 0 )
+		if (size > kMinimumDS64ChunkSize && ds64.tableLength > 0 && ((size - kMinimumDS64ChunkSize) >= (ds64.tableLength * sizeof(ChunkSize64))))
 		{
 			// copy chunk sizes table
 			//
-			XMP_Assert( size - kMinimumDS64ChunkSize >= ds64.tableLength * sizeof(ChunkSize64));
 
 			XMP_Uns32 offset = kMinimumDS64ChunkSize;
 			ChunkSize64 chunkSize;
